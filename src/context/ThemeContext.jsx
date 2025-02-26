fec61381c7df166c22134e945885755e745622e7
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2025-02-26 22:43:52 +0600

    New: Added theme context

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,42 @@
+import React, { createContext, useState, useMemo } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const ThemeContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+const ThemeContextProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  // Toggle Theme Function
+  const toggleTheme = () => {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    localStorage.setItem("theme", newMode ? "dark" : "light");
+  };
+
+  // MUI Theme Object
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: darkMode ? "dark" : "light",
+        },
+      }),
+    [darkMode]
+  );
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContextProvider;
